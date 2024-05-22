a3f4291f026449317a2c00bb386d818627485bf8
Pinier-Nicolas <pinier74nicolas@gmail.com>
2024-05-22 22:58:50 +0200

    fix/wrong file path throught a component

--- components/shared/AppButton.vue
+++ components/shared/AppButton.vue
@@ -28,7 +28,7 @@ const classes = computed(() => {
 
 <template>
  <button :disabled="props.loading" class="rounded-lg p-2 font-respira transition-all duration-300 flex gap-4 justify-center items-center" :class="classes">
-   <NuxtImg v-if="props.icon && !loading" :src="`icons/${icon}.svg`" :alt="props.label" width="20px" height="20px" />
+   <NuxtImg v-if="props.icon && !loading" :src="icon" :alt="props.label" width="20px" height="20px" />
    <Icon name="line-md:loading-loop" v-if="loading" />
    {{ props.label }}
  </button>
