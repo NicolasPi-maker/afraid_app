b0aa6aca8bf776aede9f457a3689ed5699c923a3
Pinier-Nicolas <pinier74nicolas@gmail.com>
2024-05-22 23:14:19 +0200

    fix-4/try to fix error on AppButton play svg icon

--- components/shared/AppButton.vue
+++ components/shared/AppButton.vue
@@ -28,7 +28,8 @@ const classes = computed(() => {
 
 <template>
  <button :disabled="props.loading" class="rounded-lg p-2 font-respira transition-all duration-300 flex gap-4 justify-center items-center" :class="classes">
-   <NuxtImg v-if="props.icon && !loading" :src="`icons/Play.svg`" :alt="props.label" width="20px" height="20px" />
+   <NuxtImg :src="icon" width="100"/>
+   <NuxtImg v-if="props.icon && !loading" :src="icon" :alt="props.label" width="20px" height="20px" />
    <Icon name="line-md:loading-loop" v-if="loading" />
    {{ props.label }}
  </button>
